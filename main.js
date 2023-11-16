3dec0014ec642b7232ee9d93d940a14516471cf1
HP <amjoshua59@gmail.com>
2023-11-16 20:18:58 +0000

    Making page responsive

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const listIcon = document.getElementById('listIcon');
+    const closeIcon = document.getElementById('closeIcon');
+    const iconContainer = document.getElementById('iconContainer');
+    const navLinks = document.getElementById('navLinks')
+
+    closeIcon.style.display = 'none';
+
+    listIcon.addEventListener('click', function () {
+        listIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+        navLinks.style.display = 'block';
+
+    });
+
+    closeIcon.addEventListener('click', function () {
+        closeIcon.style.display = 'none';
+        listIcon.style.display = 'block';
+        navLinks.style.display = 'none'
+    });
+});
